b2e8fe091625963e670074c63933d58ad31593a9
sneha <snehachallayil1719@gmail.com>
2023-07-12 12:20:54 +0530

    changes

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s9j^%do9)tb%2m(kk=pkhglowvl4^f!bec3*4f$=igh$nq^%&8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
